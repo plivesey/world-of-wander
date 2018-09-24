3a61cd8a4a8a38996c4e419ba432c4c882b9da50
Peter Livesey <plivesey453@gmail.com>
2018-09-24 15:43:05 -0700

    A bunch more website work

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
+import './Map.css'
+import './ComeMeetUs.css'
+import './StayUpToDate.css'
 
 import MapBackground from '../assets/mapBackground.png'
 import Iceland from '../assets/iceland.png'
@@ -7,6 +10,9 @@ import UK from '../assets/uk.png'
 import France from '../assets/france.png'
 import PNW from '../assets/pnw.png'
 import SEAsia from '../assets/seAsia.png'
+import StayWithUs from '../assets/stayWithUs.png'
+import ComeCaribbean from '../assets/comeCaribbean.png'
+import ComeAsia from '../assets/comeAsia.png'
 
 class HeaderBar extends Component {
   render() {
@@ -42,12 +48,82 @@ class Map extends Component {
   }
 }
 
+class ComeMeetUsLocationRow extends Component {
+  render() {
+    return (
+      <div class='comeMeetUsLocationRow'>
+        <a
+          class='comeMeetUsLocationRowLeft'
+          href={this.props.link} style={{ backgroundImage: "url(" + this.props.image + ")" }}
+          target="_blank"
+        >
+          Search
+        </a>
+        <div class='comeMeetUsLocationRowRight'>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.text}</p>
+          <a href={this.props.link} target="_blank">Search</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+class ComeMeetUs extends Component {
+  render() {
+    return (
+      <div id='comeMeetUs'>
+        <h2 class='header2'>COME MEET US</h2>
+        <div id='comeMeetUsText'>
+          We would love for friends and family to join us for parts of our trip! We are extremely flexible, and here are the parts of the world we’re going to next:
+        </div>
+        <div class='comeMeetUsLocationContainer'>
+          <ComeMeetUsLocationRow
+            image={ComeAsia}
+            title='Southeast Asia'
+            text='Based in Vietnam, Oct 2018 - April 2019'
+            link='https://www.google.com/flights/#flt=SFO..2018-10-10*.SFO.2018-10-14;c:USD;e:1;sd:0;er:80046474.960554778.244369736.1224226653;t:e'
+          />
+          <ComeMeetUsLocationRow
+            image={ComeCaribbean}
+            title='The Caribbean'
+            text='British Virgin Islands, April 2019'
+            link='https://www.google.com/flights/#flt=SFO..2018-10-10*.SFO.2018-10-14;c:USD;e:1;sd:0;er:140768132.-709389298.222320682.-577553360;t:e'
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+class StayUpToDate extends Component {
+  signUp() {
+    const email = 'alicecamilleavery@gmail.com'
+    const subject = 'Sign me up for World of Wander Updates'
+    const message = 'Just send the email and we\'ll add you! 😀 🎉'
+    const mailToLink = 'mailto:' + email + '?subject=' + subject + '&body=' + message
+
+    window.open(mailToLink, 'emailWindow')
+  }
+
+  render() {
+    return (
+      <div id='stayUpToDate' style={{ backgroundImage: "url(" + StayWithUs + ")" }}>
+        <h2 class='header2'>STAY UP TO DATE</h2>
+        <button onClick={() => { this.signUp() }}>Get Emails From Us</button>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HeaderBar />
         <Map />
+        <ComeMeetUs />
+        <StayUpToDate />
       </div>
     );
   }
